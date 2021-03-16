d02b4a00bbbb8831b23a0a1e4ca1227bbf724061
Phoomparin Mano <poom@hey.com>
2021-03-17 05:15:37 +0700

    chore: remove unused styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
 import '../styles/global.css'
-import '../styles/global.scss'
 
-function MyApp({Component, pageProps}) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
