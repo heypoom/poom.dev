70faf74112d701fcdfd7247b680a0c8068bc3483
Phoomparin Mano <poom@hey.com>
2021-03-17 05:13:54 +0700

    feat: add button variant demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
-import tw from 'twin.macro'
+import tw, { styled, TwStyle } from 'twin.macro'
 
 const Banner = tw.div`p-5 bg-purple-500 text-white text-2xl shadow-lg`
-const Button = tw.button`bg-purple-500 px-5 py-2 text-white text-center`
+
+type ButtonVariant = 'primary' | 'important'
+
+interface ButtonProps {
+  variant: ButtonVariant
+}
+
+const bgByVariantMap: Record<ButtonVariant, TwStyle> = {
+  primary: tw`bg-purple-500 hover:bg-purple-600`,
+  important: tw`bg-red-500 hover:bg-red-700`,
+}
+
+const Button = styled.button<ButtonProps>(({ variant }) => [
+  variant && bgByVariantMap[variant],
+  tw`px-5 py-2 text-white text-center`,
+])
 
 const Home: React.FC = () => (
   <main>
@@ -10,12 +25,18 @@ const Home: React.FC = () => (
     </Banner>
 
     <div tw="min-h-screen flex items-center">
-      <div tw="px-5 my-10 max-w-3xl mx-auto space-y-6">
+      <div tw="px-2 my-10 max-w-3xl mx-auto space-y-6">
         <h1 tw="text-5xl font-bold">Hey, There!</h1>
 
         <p tw="font-light">Glad that you're here.</p>
 
-        <Button tw="font-medium">Learn More</Button>
+        <div tw="flex space-x-3">
+          <Button variant="primary">Keep</Button>
+
+          <Button variant="important" tw="font-bold">
+            Delete
+          </Button>
+        </div>
       </div>
     </div>
   </main>
