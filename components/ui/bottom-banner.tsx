0b4778850610f7877b816a3acba5e1e9eaaf1054
Phoomparin Mano <poom@hey.com>
2021-03-17 23:44:59 +0700

    feat: use tailwind jit

--- components/ui/bottom-banner.tsx
+++ components/ui/bottom-banner.tsx
@@ -14,9 +14,9 @@ export const BottomBanner = () => {
                   stroke="currentColor"
                   aria-hidden="true">
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"
                   />
                 </svg>
@@ -32,7 +32,7 @@ export const BottomBanner = () => {
               <a
                 href="#"
                 tw="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50">
-                Learn more
+                Learn more!
               </a>
             </div>
             <div tw="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
@@ -49,9 +49,9 @@ export const BottomBanner = () => {
                   stroke="currentColor"
                   aria-hidden="true">
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
