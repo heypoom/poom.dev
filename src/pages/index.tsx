5d1094a1641e0572de47636d86a710f1f03d7044
Phoomparin Mano <poom@hey.com>
2021-10-03 12:15:51 +0700

    chore: links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,38 @@ import tw from "twin.macro"
 import { styled } from "goober"
 
 const Backdrop = tw.main`
-  flex items-center justify-center bg-purple-500
-  min-h-screen px-2
+  flex flex-col items-center justify-center bg-purple-500
+  min-h-screen px-2 text-white
 `
 
 const Title = tw.h1`
-  text-center px-2 text-white
-  text-xl xs:text-3xl sm:text-5xl md:text-6xl lg:text-8xl
+  text-center px-2 text-white my-0
+  text-2xl xs:text-4xl sm:text-5xl md:text-6xl lg:text-8xl
 `
+
+const Link: React.FC<{ href: string }> = ({ href, children }) => (
+  <a
+    tw="text-white text-xs xs:text-sm sm:text-lg"
+    target="_blank"
+    rel="noreferrer"
+    href={href}
+  >
+    {children}
+  </a>
+)
 
 const Landing = () => {
   return (
-    <Backdrop>
-      <Title>Phoomparin Mano</Title>
+    <Backdrop tw="space-y-5">
+      <Title tw="md:mb-4">Phoomparin Mano</Title>
+
+      <p tw="text-xl">@heypoom</p>
+
+      <section tw="flex space-x-2">
+        <Link href="https://github.com/heypoom">GitHub</Link>
+        <Link href="https://facebook.com/phoomparin.mano">Facebook</Link>
+        <Link href="https://twitter.com/heypoom">Twitter</Link>
+      </section>
     </Backdrop>
   )
 }
