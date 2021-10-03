58be4a8591a5ef0f53cbf97c0e49abdcf0a43be2
Phoomparin Mano <poom@hey.com>
2021-10-03 12:06:49 +0700

    refactor: formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,13 @@ import tw from "twin.macro"
 import { styled } from "goober"
 
 const Backdrop = tw.main`
-	flex items-center justify-center
-	 bg-purple-500
-	min-h-screen px-2
+  flex items-center justify-center bg-purple-500
+  min-h-screen px-2
 `
 
 const Title = tw.h1`
-	text-white text-center px-2
-	text-xl xs:text-3xl sm:text-5xl md:text-6xl lg:text-8xl
+  text-center px-2 text-white
+  text-xl xs:text-3xl sm:text-5xl md:text-6xl lg:text-8xl
 `
 
 const Landing = () => {
