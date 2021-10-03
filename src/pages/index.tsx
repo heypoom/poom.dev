443332b62dba51aa623e7709a5b309db8c18b8b6
Phoomparin Mano <poom@hey.com>
2021-10-03 12:04:25 +0700

    feat: responsive design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import tw from "twin.macro"
 import { styled } from "goober"
 
-const Title = tw.div`
+const Backdrop = tw.main`
 	flex items-center justify-center
-	text-white text-8xl bg-purple-500
+	 bg-purple-500
+	min-h-screen px-2
+`
+
+const Title = tw.h1`
+	text-white text-center px-2
+	text-xl xs:text-3xl sm:text-5xl md:text-6xl lg:text-8xl
 `
 
 const Landing = () => {
-  return <Title tw="min-h-screen">Phoomparin Mano</Title>
+  return (
+    <Backdrop>
+      <Title>Phoomparin Mano</Title>
+    </Backdrop>
+  )
 }
 
 export default Landing
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   darkMode: "media",
   theme: {
     extend: {},
+    screens: {
+      xs: "320px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1440px",
+    },
   },
   variants: {
     extend: {},
