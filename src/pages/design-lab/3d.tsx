2725653d865092f48d417f2dee25258574811709
Phoomparin Mano <poom@hey.com>
2021-12-19 20:07:42 +0700

    feat: render bookshelf

--- src/pages/design-lab/3d.tsx
+++ src/pages/design-lab/3d.tsx
@@ -1,69 +1,11 @@
-import { FC, useRef, useState } from "react"
-import { Mesh } from "three"
-import { Canvas, Vector3, useFrame } from "@react-three/fiber"
-import { useSpring, a } from "@react-spring/three"
+import dynamic from "next/dynamic"
 
-import tw from "twin.macro"
+const BookshelfView = dynamic(() => import("../../three-views/BookshelfView"), {
+  ssr: false,
+})
 
-const Backdrop = tw.div`
- bg-gray-900 min-h-screen w-full
-`
-
-interface BoxProps {
-  color?: string
-  hoverColor?: string
-  position: Vector3
+const Page = () => {
+  return <BookshelfView />
 }
 
-function Box(props: BoxProps) {
-  const { color = "orange", hoverColor = "hotpink", position } = props
-
-  const meshRef = useRef<Mesh>()
-  const [hovering, hover] = useState(false)
-
-  const values = useSpring({
-    scale: hovering ? 1.5 : 1,
-    color: hovering ? hoverColor : color,
-  })
-
-  useFrame(() => {
-    if (!meshRef.current) return
-
-    meshRef.current.rotation.x += 0.01
-    meshRef.current.rotation.z += 0.01
-  })
-
-  return (
-    <a.mesh
-      ref={meshRef}
-      position={position}
-      scale={values.scale}
-      onPointerOver={() => hover(true)}
-      onPointerLeave={() => hover(false)}
-    >
-      <icosahedronGeometry args={[1.25, 0]} />
-      <a.meshStandardMaterial color={values.color} />
-    </a.mesh>
-  )
-}
-
-const ThreeView = () => {
-  return (
-    <Backdrop>
-      <Canvas
-        tw="w-full min-h-screen"
-        dpr={[1, 2]}
-        gl={{ powerPreference: "high-performance" }}
-      >
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
-
-        <Box position={[-2.7, 0, 0]} color="#ff4757" hoverColor="#5352ed" />
-        <Box position={[2.7, 0, 0]} color="#ff6348" hoverColor="#2ed573" />
-      </Canvas>
-    </Backdrop>
-  )
-}
-
-export default ThreeView
+export default Page
