f837ae0549be7d749dfa340724f586eab60f49ff
Phoomparin Mano <poom@hey.com>
2022-12-29 17:11:33 +0700

    chore: title

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = 'Phoomparin Mano - Poom'
+
+export const SITE_DESCRIPTION =
+  'Phoomparin Mano is a software engineer based in Bangkok, Thailand.'
