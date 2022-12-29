ac4ad8a335df1e05b7b2696cb9df82be9e4a696d
Phoomparin Mano <poom@hey.com>
2022-12-29 19:16:32 +0700

    chore: chunk optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,27 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://poom.dev',
+
+  vite: {
+    build: {
+      rollupOptions: {
+        treeshake: {
+          moduleSideEffects: false,
+        },
+        output: {
+          manualChunks(id) {
+            if (/threlte|troika/.test(id)) return 'threlte'
+            if (/three\/examples\/jsm/.test(id)) return 'three-extras'
+            if (/three/.test(id)) return 'three'
+            if (/node_modules/.test(id)) return 'vendor'
+
+            return 'main'
+          },
+        },
+      },
+    },
+  },
+
   integrations: [
     mdx(),
     sitemap(),
