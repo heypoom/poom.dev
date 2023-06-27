386258ba9cab187074a80b74543c355e97e7bbdd
Phoomparin Mano <phoomparin@gmail.com>
2023-06-27 18:27:56 +0700

    feat: separate verify script

--- cmd/verify.ts
+++ cmd/verify.ts
@@ -1,15 +1,8 @@
-import { existsSync } from 'fs'
-
-import { cloneRepository } from './prepare/clone'
 import { processMarkdownFiles } from './prepare/process'
 import { verifyBacklinks } from './prepare/verify'
 
-import { NOTES_DIR } from './prepare/constants'
 import { timed as t } from './prepare/timed'
-
-if (!existsSync(NOTES_DIR)) {
-  t('clone', cloneRepository)
-}
+import { NOTES_DIR } from './prepare/constants'
 
 const contents = await t('process', () => processMarkdownFiles(NOTES_DIR))
 t('verify', () => verifyBacklinks(contents))
