5bf47c5573f1d8ecd9ef22343af6f6aa39893507
Phoomparin Mano <phoomparin@gmail.com>
2023-06-27 18:26:16 +0700

    chore: warn

--- cmd/prepare/verify.ts
+++ cmd/prepare/verify.ts
@@ -1,14 +1,12 @@
 import type { Content } from './types'
 
 export function verifyBacklinks(contents: Content[]) {
-  const missing: [string, string][] = []
-
   for (const content of contents) {
     for (const link of content.links) {
       const referenced = contents.find((c) => c.name === link)
 
       if (!referenced) {
-        console.log(`${content.name} -> ${link}: broken link!`)
+        console.warn(`${content.name} -> ${link}: broken link!`)
       }
     }
   }
