876a0ed7857ff5c2547601ef66f8a8eb7514c535
Phoomparin Mano <phoomparin@gmail.com>
2023-06-27 18:46:42 +0700

    feat: track reference in the graph

--- cmd/prepare/constants.ts
+++ cmd/prepare/constants.ts
@@ -8,3 +8,6 @@ export const GITHUB_REPO = 'heypoom/notes'
 // Where to clone the digital garden repository to.
 export const CLONE_DIR = 'data'
 export const NOTES_DIR = `${CLONE_DIR}/notes`
+
+// Name of your markdown index or home page.
+export const HOME_NAME = 'Home'
--- cmd/prepare/verify.ts
+++ cmd/prepare/verify.ts
@@ -1,13 +1,32 @@
+import { HOME_NAME } from './constants'
+
 import type { Content } from './types'
 
 export function verifyReferences(contents: Content[]) {
-  for (const content of contents) {
-    for (const link of content.links) {
-      const referenced = contents.find((c) => c.name === link)
+  const graph = new Map<string, Set<string>>()
 
-      if (!referenced) {
-        console.warn(`${content.name} -> ${link}: broken link!`)
+  for (const parent of contents) {
+    for (const link of parent.links) {
+      // Does all links in the content exist?
+      const child = contents.find((c) => c.name === link)
+
+      if (!child) {
+        console.warn(`${parent.name} -> ${link}: inaccessible!`)
+        continue
+      }
+
+      // Track the reference in the graph.
+      if (graph.has(parent.path)) {
+        graph.get(parent.path)?.add(child.path)
+      } else {
+        graph.set(parent.path, new Set([child.path]))
       }
     }
   }
+
+  console.log(graph)
+
+  // for (const content of contents) {
+  //   console.warn(`${content.name}: never referenced!`)
+  // }
 }
