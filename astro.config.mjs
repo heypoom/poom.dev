3e9f40dc3cf9899f4f41c5261ec766d59ab2864a
Phoomparin Mano <phoomparin@gmail.com>
2023-07-03 01:22:09 +0700

    feat: update to astro 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
