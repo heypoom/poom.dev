ecd3370531056fc970432a7d7f00017f29a2e7bf
Phoomparin Mano <phoomparin@gmail.com>
2023-07-03 01:27:40 +0700

    feat: vercel hybrid adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
 import tailwind from '@astrojs/tailwind'
+
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://poom.dev',
   integrations: [mdx(), sitemap(), tailwind()],
+  output: 'hybrid',
+  adapter: vercel(),
 })
