cb55a26b3f483ef4b56117ca490a3c2caba8177a
Phoomparin Mano <phoomparin@gmail.com>
2023-07-03 01:01:45 +0700

    fix: remove svelte temporarily

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,10 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-import svelte from '@astrojs/svelte'
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://poom.dev',
-  integrations: [mdx(), sitemap(), svelte(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind()],
 })
