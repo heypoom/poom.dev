dbaa334a1958d688b86b396bb664667388c80985
Phoomparin Mano <phoomparin@gmail.com>
2023-12-28 20:09:41 +0700

    feat: site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
-import partytown from "@astrojs/partytown";
+import partytown from "@astrojs/partytown"
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://poom.dev',
-  integrations: [mdx(), sitemap(), tailwind(), partytown()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    partytown()
+  ],
   output: 'hybrid',
   adapter: vercel()
-});
+})
+
