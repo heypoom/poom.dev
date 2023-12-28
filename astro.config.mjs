4873acf0aedadee5433d7f6c5bd223d1e553c933
Phoomparin Mano <phoomparin@gmail.com>
2023-12-28 19:38:57 +0700

    feat: add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
-import vercel from '@astrojs/vercel/serverless'
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://poom.dev',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), partytown()],
   output: 'hybrid',
-  adapter: vercel(),
-})
+  adapter: vercel()
+});
