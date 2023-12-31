ac969f9da24c9deee8cf9d8475e957188d23929f
Phoomparin Mano <phoomparin@gmail.com>
2023-12-31 23:42:40 +0700

    feat: v2 blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,20 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
+import partytown from "@astrojs/partytown";
 
-import partytown from "@astrojs/partytown"
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://poom.dev',
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      applyBaseStyles: true,
-    }),
-    partytown()
-  ],
+  integrations: [mdx(), sitemap(), tailwind({
+    applyBaseStyles: true
+  }), partytown(), icon()],
   output: 'hybrid',
-  adapter: node({mode: 'standalone'}),
-})
-
+  adapter: node({
+    mode: 'standalone'
+  })
+});
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Hanken Grotesk'],
+      mono: ['IBM Plex Mono'],
+    },
   },
   plugins: [
     require('@tailwindcss/typography')
