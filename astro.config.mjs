026b248f7dc18730a51b9df9dc36827d036022e7
Phoomparin Mano <phoomparin@gmail.com>
2024-01-03 17:09:14 +0700

    chore: add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-import partytown from "@astrojs/partytown";
-
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import partytown from '@astrojs/partytown'
+import cloudflare from '@astrojs/cloudflare'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://poom.dev',
-  integrations: [mdx(), sitemap(), tailwind({
-    applyBaseStyles: true
-  }), partytown(), icon()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: true,
+    }),
+    partytown(),
+    icon(),
+  ],
   output: 'server',
-  adapter: vercel(),
-});
+  adapter: cloudflare(),
+})
