7db1896f277551131175b0f9eb5fb49eed985a6f
Phoomparin Mano <phoomparin@gmail.com>
2024-01-04 00:28:50 +0700

    feat: static builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,24 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-import partytown from "@astrojs/partytown";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import partytown from '@astrojs/partytown'
 
-import icon from "astro-icon";
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://poom.dev',
-  integrations: [mdx(), sitemap(), tailwind({
-    applyBaseStyles: true
-  }), partytown(), icon()],
-  output: 'server',
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: true,
+    }),
+    partytown(),
+    icon(),
+  ],
+  output: 'hybrid',
   adapter: vercel(),
-});
+})
